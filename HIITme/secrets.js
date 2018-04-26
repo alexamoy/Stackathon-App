205c2f23236f54d18e362c94dd460e7e3d8c2224
alexamoy <35125717+alexamoy@users.noreply.github.com>
2018-04-26 11:48:00 -0400

    Delete secrets.js

--- HIITme/secrets.js
+++
@@ -1,3 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '620522442279-9g6f5pumsla5vs95gqios3nasg0cu4mr.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'K2BDTAUgXrfRm0u4D9Wf3Rif'
-process.env.GOOGLE_CALLBACK = 'http://localhost:8080/auth/google'
